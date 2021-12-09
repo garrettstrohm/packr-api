6156acccae9e566540adec0fd163335969cc0442
Garrett <garres4@vt.edu>
2021-12-09 16:23:49 -0700

    changed user creation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,7 +13,7 @@ class UserController < Sinatra::Base
     end
 
     post '/users' do
-        new_user = User.create(username: params[:username], first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone: params[:phone])
+        new_user = User.first_or_create(username: params[:username], first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone: params[:phone])
         new_user.to_json
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
     has_many :trips
     has_many :trip_items, through: :trips
     has_many :adventures
+
+
 end
 
 
